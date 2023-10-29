c81cc62d86ea1c7a28a50d4401d0097fbd44a86f
Habtamu <habtamuwolde92@gmail.com>
2023-10-29 15:30:14 +0530

    added base for build

--- result_summary/vite.config.js
+++ result_summary/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://github.com/habtew/frontendmentor.io/tree/main/result_summary',
 })
