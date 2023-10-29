469fbc5e871015ef1aeca591008758c0223d5e2b
Habtamu <habtamuwolde92@gmail.com>
2023-10-29 15:37:06 +0530

    adding dist file

--- result_summary/vite.config.js
+++ result_summary/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'frontendmentor.io/',
   plugins: [react()],
-  base:'https://github.com/habtew/frontendmentor.io/tree/main/result_summary',
 })
