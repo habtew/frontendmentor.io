e1b2b58d58442ae2cea8d059f91a4ac09a2db000
Habtamu <habtamuwolde92@gmail.com>
2023-10-30 14:13:03 +0530

    renamed to main.jsx and added base location

--- result_summary/vite.config.js
+++ result_summary/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/frontendmentor.io/',
+  base:'/frontendmentor.io/result_summary/',
   plugins: [react()],
 })
